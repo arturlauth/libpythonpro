e66c3b097e96553e1efa06dcb0b5da4969b54475
arturlauth-dev <arturlauthmain@gmail.com>
2020-04-20 17:50:55 -0300

    criação do pacote, e função buscar_avatar

---
+++ libpythonpro/github_api.py
@@ -0,0 +1,21 @@
+import requests
+
+
+def buscar_avatar(usuario):
+    """
+    Busca o avatar de um usuário no Github
+    Parameters
+    ----------
+    usuario : str com o nome de usuário no github
+
+    Returns
+    -------
+    str com o link do avatar
+
+    """
+    url = f'https://api.github.com/users/{usuario}'
+    resp = requests.get(url)
+    return resp.json() ['avatar_url']
+
+if __name__ == '__main__':
+    print(buscar_avatar('arturlauth-dev'))
