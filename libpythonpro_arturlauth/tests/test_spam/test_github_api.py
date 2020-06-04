bdcd53289c5f2888d1e6956e01d7c616b3811086
arturlauth-dev <arturlauthmain@gmail.com>
2020-06-04 12:09:52 -0300

    Utilizando o mocker - (Biblioteca Pytest-Mock)

--- libpythonpro_arturlauth/tests/test_spam/test_github_api.py
+++ libpythonpro_arturlauth/tests/test_spam/test_github_api.py
@@ -6,15 +6,14 @@ from libpythonpro_arturlauth import github_api
 
 
 @pytest.fixture
-def avatar_url():
+def avatar_url(mocker):
     resp_mock = Mock()
     url = 'https://avatars2.githubusercontent.com/u/63745619?v=4'
     resp_mock.json.return_value = {'login': 'arturlauth', 'id': 63745619,
                                    'avatar_url': url, }
-    get_original = github_api.requests.get
-    github_api.requests.get = Mock(return_value=resp_mock)
-    yield url
-    github_api.requests.get = get_original
+    get_mock = mocker.patch('libpythonpro_arturlauth.github_api.requests.get')
+    get_mock.return_value = resp_mock
+    return url
 
 
 def test_buscar_avatar(avatar_url):
